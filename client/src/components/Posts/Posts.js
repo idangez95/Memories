dd767af7f13e5b43da927329183dcea499f6cdf6
idangez95 <idangez95@gmail.com>
2022-09-11 02:13:44 +0300

    adding question mark before map

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -13,7 +13,7 @@ const Posts = ({setCurrentId}) => {
     return (
         !posts.length ? <CircularProgress /> : (
             <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-                {posts.map((post) => (
+                {posts?.map((post) => (
                     // eslint-disable-next-line react/jsx-no-duplicate-props
                     <Grid item key={post._id} item xs={12} sm={6}> 
                         <Post post={post} setCurrentId={setCurrentId} />
