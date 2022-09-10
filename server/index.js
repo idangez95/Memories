ff7a7264f11fb9f79c7a552aea8da4faa1f939a3
idangez95 <idangez95@gmail.com>
2022-09-11 01:30:09 +0300

    update app

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config()
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors())
+app.use(cors());
 
 app.use('/posts', postRoutes)
 
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
-.catch((error) => console.log(error.message))
+    .then(() => app.listen(PORT, () => console.log(`Server running on port ${PORT}`)))
+    .catch((error) => console.log(error.message))
 
 mongoose.set('useFindAndModify', false)
