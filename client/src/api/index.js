ecb0157fcddbe06d6d09288c4da260870ccb221e
idangez95 <idangez95@gmail.com>
2022-09-11 11:54:06 +0300

    remove posts back slash

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'https://idan-mini-memories.netlify.app/posts'
+const url = 'https://idan-mini-memories.netlify.app'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
