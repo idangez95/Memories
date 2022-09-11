12a91cfa46937fc2c70d3cc69d510531d561102f
idangez95 <idangez95@gmail.com>
2022-09-11 12:11:50 +0300

    changing mongo uri

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'https://idan-mini-memories.netlify.app'
+const url = 'https://idan-mini-memories.netlify.app/posts'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
