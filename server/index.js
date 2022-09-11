6dd8c3bd6418192460ee9a530d8367df327af6e2
idangez95 <idangez95@gmail.com>
2022-09-11 12:17:11 +0300

    remove posts from url

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config()
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors())
+app.use(cors('*'))
 
 app.use('/posts', postRoutes)
 
