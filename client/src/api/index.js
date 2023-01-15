de2c57afb8fa7fe7e408dcf1ba6ffd101d2f5b7e
idangez95 <idangez95@gmail.com>
2023-01-16 00:22:59 +0200

    solve bug of posts.maps

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const url = 'https://memories-t65q.onrender.com'
+const url = 'https://memories-t65q.onrender.com/posts'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
