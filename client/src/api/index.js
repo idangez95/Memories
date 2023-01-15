db9b2d99d004eef38aa53c60fffe03c99fc428f6
idangez95 <idangez95@gmail.com>
2023-01-16 00:09:15 +0200

    replace heroku with render

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
-// const url = 'https://idan-mini-memories.netlify.app'
-const url = 'https://localhost:5000/posts'
+const url = 'https://memories-t65q.onrender.com'
 
 export const fetchPosts = () => axios.get(url)
 export const createPost = (newPost) => axios.post(url, newPost)
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors('*'))
 app.use('/posts', postRoutes)
 
 app.get('/', (req, res) => {
-    res.send('Hey')
+    res.send('Hello to Memories API')
 })
 
 const PORT = process.env.PORT || 5000
